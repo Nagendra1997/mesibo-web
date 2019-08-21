14b68fe524808bc0524090798401c5a20be112a4
Nagendra <nagynv@gmail.com>
2019-08-21 13:02:35 +0530

    Formating

--- scripts/localstorage.js
+++ scripts/localstorage.js
@@ -1,97 +1,106 @@
 //localstorage.js
 
-function LocalStorage_LoadHistory(selected_user){
-      console.log("===>LocalStorage_LoadHistory called")
-  
-      msg_history=JSON.parse(localStorage.getItem(selected_user));
+function LocalStorage_LoadHistory(selected_user) {
+  console.log("===>LocalStorage_LoadHistory called")
 
-      if(msg_history){
+  msg_history = JSON.parse(localStorage.getItem(selected_user));
 
-      var msg_hist_data=Object.keys(msg_history).map(function(key){return msg_history[key];});
-      // msg_hist_data.sort(function(a,b){return a.ts - b.ts;});
+  if (msg_history) {
 
-      var previous_date=0;
+    var msg_hist_data = Object.keys(msg_history).map(function(key) {
+      return msg_history[key];
+    });
+    // msg_hist_data.sort(function(a,b){return a.ts - b.ts;});
 
-      for (var i=0;i<msg_hist_data.length;i++){
+    var previous_date = 0;
 
-        var msg_data = msg_hist_data[i];
-        previous_date =createDateHeader_forHistory(msg_data,previous_date);
+    for (var i = 0; i < msg_hist_data.length; i++) {
+      var msg_data = msg_hist_data[i];
+      previous_date = createDateHeader_forHistory(msg_data, previous_date);
 
-        if(msg_data['flag'] == 0)
-          createSentBubble(msg_data); 
-        else
-          createRecievedBubble(msg_data);
-        }
-
+      if (msg_data['flag'] == 0)
+        createSentBubble(msg_data);
+      else
+        createRecievedBubble(msg_data);
     }
+
+  }
 }
 
 //m is the message object
 
-function LocalStorage_UpdateItemSent(m){
+function LocalStorage_UpdateItemSent(m) {
   // console.log(m);
   console.log("===>LocalStorage_UpdateItemSent called");
-  var retrievedMsgArray = localStorage.getItem(m.peer); 
+  var retrievedMsgArray = localStorage.getItem(m.peer);
   //MsgList Entry Will always exist if msg sent.No need to check
-  var jsonMsgArray=JSON.parse(retrievedMsgArray);
+  var jsonMsgArray = JSON.parse(retrievedMsgArray);
   // console.log(jsonMsgArray);
 
-  for (var i=jsonMsgArray.length-1;i>=0;i--){
-    if(jsonMsgArray[i]['id']== m.id){
-      jsonMsgArray[i]['params']=m;
-      jsonMsgArray[i]['status']=m.status;
-      }
+  for (var i = jsonMsgArray.length - 1; i >= 0; i--) {
+    if (jsonMsgArray[i]['id'] == m.id) {
+      jsonMsgArray[i]['params'] = m;
+      jsonMsgArray[i]['status'] = m.status;
     }
-        
-  localStorage.setItem(m.peer,JSON.stringify(jsonMsgArray));
+  }
+
+  localStorage.setItem(m.peer, JSON.stringify(jsonMsgArray));
 
 }
 
-function LocalStorage_UpdateItemRecieved(m,string){
+function LocalStorage_UpdateItemRecieved(m, string) {
   console.log("===>LocalStorage_UpdateItemRecieved called");
-  var retrievedMsgArray  = localStorage.getItem(m.peer);
+  var retrievedMsgArray = localStorage.getItem(m.peer);
   var jsonMsgArray = [];
 
-    if(retrievedMsgArray)
-      jsonMsgArray=JSON.parse(retrievedMsgArray);
-   
-    jsonMsgArray.push({'id':m.id,'peer':m.peer,'params':m,'data':string,'groupid':0,'ts':+ new Date,'flag':m.flag});
-    localStorage.setItem(m.peer,JSON.stringify(jsonMsgArray));
+  if (retrievedMsgArray)
+    jsonMsgArray = JSON.parse(retrievedMsgArray);
+
+  jsonMsgArray.push({
+    'id': m.id,
+    'peer': m.peer,
+    'params': m,
+    'data': string,
+    'groupid': 0,
+    'ts': +new Date,
+    'flag': m.flag
+  });
+  localStorage.setItem(m.peer, JSON.stringify(jsonMsgArray));
 }
 
-function LocalStorage_NewItemSent(id,peer,msg_payload){
+function LocalStorage_NewItemSent(id, peer, msg_payload) {
   console.log("===>LocalStorage_NewItemSent called");
   var retrievedMsgArray = localStorage.getItem(peer);
-      
-  if(retrievedMsgArray)
-    retrievedMsgArray=JSON.parse(retrievedMsgArray);
+
+  if (retrievedMsgArray)
+    retrievedMsgArray = JSON.parse(retrievedMsgArray);
   else
-    retrievedMsgArray=[];
+    retrievedMsgArray = [];
 
   retrievedMsgArray.push(msg_payload);
-  localStorage.setItem(peer,JSON.stringify(retrievedMsgArray));
+  localStorage.setItem(peer, JSON.stringify(retrievedMsgArray));
 
   console.log(JSON.parse(localStorage.getItem(peer)));
 }
 
 
-function LocalSorage_MsgPeerHash(msgId,peer){
+function LocalSorage_MsgPeerHash(msgId, peer) {
   var retrievedMsgHash = localStorage.getItem("Mesibo_MsgUsr_Hash");
-  if(retrievedMsgHash)
+  if (retrievedMsgHash)
     retrievedMsgHash = JSON.parse(retrievedMsgHash);
   else
     retrievedMsgHash = {};
 
-    retrievedMsgHash[msgId]=peer;
-    localStorage.setItem("Mesibo_MsgUsr_Hash",JSON.stringify(retrievedMsgHash));
+  retrievedMsgHash[msgId] = peer;
+  localStorage.setItem("Mesibo_MsgUsr_Hash", JSON.stringify(retrievedMsgHash));
 
-  
+
 }
 
-function LocalStorage_GetPeerFromId(msgId){
+function LocalStorage_GetPeerFromId(msgId) {
 
   var retrievedMsgHash = localStorage.getItem("Mesibo_MsgUsr_Hash");
-  if(retrievedMsgHash)
+  if (retrievedMsgHash)
     retrievedMsgHash = JSON.parse(retrievedMsgHash);
   else {
     console.log("Error: Invalid message ID");
@@ -101,4 +110,3 @@ function LocalStorage_GetPeerFromId(msgId){
   return retrievedMsgHash[msgId];
 
 }
-
--- scripts/utils.js
+++ scripts/utils.js
@@ -1,69 +1,60 @@
 //utils.js      
-      const MESIBO_READFLAG_READRECEIPT = 1;
-      const MESIBO_READFLAG_SENDLAST = 2;
-      const MESIBO_READFLAG_FIFO = 4;
-      const MESIBO_READFLAG_SUMMARY = 0x10;
-      const MESIBO_READFLAG_SENDEOR = 0x20;
-      const MESIBO_READFLAG_WITHFILES = 0x80;
+const MESIBO_READFLAG_READRECEIPT = 1;
+const MESIBO_READFLAG_SENDLAST = 2;
+const MESIBO_READFLAG_FIFO = 4;
+const MESIBO_READFLAG_SUMMARY = 0x10;
+const MESIBO_READFLAG_SENDEOR = 0x20;
+const MESIBO_READFLAG_WITHFILES = 0x80;
 
-      const MESIBO_MSGSTATUS_OUTBOX         =0
-      const MESIBO_MSGSTATUS_SENT           =1
-      const MESIBO_MSGSTATUS_DELIVERED      =2
-      const MESIBO_MSGSTATUS_READ           =3
-      const MESIBO_MSGSTATUS_RECEIVEDNEW    =0x12
-      const MESIBO_MSGSTATUS_RECEIVEDREAD   =0x13
-      const MESIBO_MSGSTATUS_CALLMISSED     =0x15
-      const MESIBO_MSGSTATUS_CALLINCOMING   =0x16
-      const MESIBO_MSGSTATUS_CALLOUTGOING   =0x17
-      const MESIBO_MSGSTATUS_CUSTOM         =0x20
+const MESIBO_MSGSTATUS_OUTBOX = 0
+const MESIBO_MSGSTATUS_SENT = 1
+const MESIBO_MSGSTATUS_DELIVERED = 2
+const MESIBO_MSGSTATUS_READ = 3
+const MESIBO_MSGSTATUS_RECEIVEDNEW = 0x12
+const MESIBO_MSGSTATUS_RECEIVEDREAD = 0x13
+const MESIBO_MSGSTATUS_CALLMISSED = 0x15
+const MESIBO_MSGSTATUS_CALLINCOMING = 0x16
+const MESIBO_MSGSTATUS_CALLOUTGOING = 0x17
+const MESIBO_MSGSTATUS_CUSTOM = 0x20
 
-      const MESIBO_MSGSTATUS_FAIL           =0x80
-      const MESIBO_MSGSTATUS_USEROFFLINE    =0x81
-      const MESIBO_MSGSTATUS_INBOXFULL      =0x82
-      const MESIBO_MSGSTATUS_INVALIDDEST    =0x83
-      const MESIBO_MSGSTATUS_EXPIRED        =0x84
-      const MESIBO_MSGSTATUS_BLOCKED        =0x88
-
-
-      const MESIBO_ORIGIN_REALTIME = 0;
-      const MESIBO_ORIGIN_DBMESSAGE = 1;
-      const MESIBO_ORIGIN_DBSUMMARY = 2;
-      const MESIBO_ORIGIN_DBPENDING = 3;
-      const MESIBO_ORIGIN_FILTER = 4;
-
-    
-
-      function timeNow() {
-        
-        var d = new Date();
-        h = (d.getHours()<10?'0':'') + d.getHours();
-        m = (d.getMinutes()<10?'0':'') + d.getMinutes();
-        return  h + ':' + m;
-      }
-
-      function timeNow2(ts){
-        var theDate = new Date(ts);
-        dateString = theDate.toLocaleTimeString();
-        return dateString.slice(0,5);
-      }
-
-      function dateNow(ts){
-        var theDate = new Date(ts);
-        dateString = theDate.toString();
-        return dateString.slice(0,4)+', '+dateString.slice(4,10);
-      }
-
-      function dateYesterday(ts){
-        var today = new Date(ts);
-        var yesterday = new Date(today);
-        yesterday.setDate(today.getDate() - 1);
-        yesterdayString = yesterday.toString();
-
-        return yesterdayString.slice(0,4)+', '+ yesterdayString.slice(4,10);
-      }
+const MESIBO_MSGSTATUS_FAIL = 0x80
+const MESIBO_MSGSTATUS_USEROFFLINE = 0x81
+const MESIBO_MSGSTATUS_INBOXFULL = 0x82
+const MESIBO_MSGSTATUS_INVALIDDEST = 0x83
+const MESIBO_MSGSTATUS_EXPIRED = 0x84
+const MESIBO_MSGSTATUS_BLOCKED = 0x88
 
 
-function createRecievedBubble(msg_data){
+function timeNow() {
+  var d = new Date();
+  h = (d.getHours() < 10 ? '0' : '') + d.getHours();
+  m = (d.getMinutes() < 10 ? '0' : '') + d.getMinutes();
+  return h + ':' + m;
+}
+
+function timeFromTs(ts) {
+  var theDate = new Date(ts);
+  dateString = theDate.toLocaleTimeString();
+  return dateString.slice(0, 5);
+}
+
+function dateNow(ts) {
+  var theDate = new Date(ts);
+  dateString = theDate.toString();
+  return dateString.slice(0, 4) + ', ' + dateString.slice(4, 10);
+}
+
+function dateYesterday(ts) {
+  var today = new Date(ts);
+  var yesterday = new Date(today);
+  yesterday.setDate(today.getDate() - 1);
+  yesterdayString = yesterday.toString();
+
+  return yesterdayString.slice(0, 4) + ', ' + yesterdayString.slice(4, 10);
+}
+
+
+function createRecievedBubble(msg_data) {
   var msgBodyDiv = document.createElement('div');
   msgBodyDiv.className = "row message-body";
   var topReceiverDiv = document.createElement('div');
@@ -71,12 +62,12 @@ function createRecievedBubble(msg_data){
   var receiverDiv = document.createElement('div');
   receiverDiv.className = 'receiver';
   var textDiv = document.createElement('div');
-  textDiv.className= 'message-text';
+  textDiv.className = 'message-text';
   var timeSpan = document.createElement('span');
   timeSpan.className = 'message-time pull-right';
-  
-  var msgcontent=document.createTextNode(msg_data['data']);
-  var timecontent=document.createTextNode(timeNow2(msg_data['ts']));
+
+  var msgcontent = document.createTextNode(msg_data['data']);
+  var timecontent = document.createTextNode(timeFromTs(msg_data['ts']));
 
   textDiv.append(msgcontent);
   timeSpan.append(timecontent);
@@ -88,9 +79,9 @@ function createRecievedBubble(msg_data){
   var mylist = document.getElementById("conversation");
   mylist.appendChild(msgBodyDiv);
 
-  }
+}
 
-function createSentBubble(msg_data){
+function createSentBubble(msg_data) {
 
   var msgBodyDiv = document.createElement('div');
   msgBodyDiv.className = "row message-body";
@@ -99,17 +90,17 @@ function createSentBubble(msg_data){
   var senderDiv = document.createElement('div');
   senderDiv.className = 'sender';
   var textDiv = document.createElement('div');
-  textDiv.className= 'message-text';
+  textDiv.className = 'message-text';
   var timeSpan = document.createElement('span');
   timeSpan.className = 'message-time pull-right';
 
   var statusTick = document.createElement('img');
   statusTick.className = 'status_msg_img';
-  updateStatusTick(statusTick,msg_data['status']);
-  statusTick.setAttribute("id",msg_data['id']);
-  
-  var msgcontent=document.createTextNode(msg_data['data']);
-  var timecontent=document.createTextNode(timeNow2(msg_data['ts']));
+  updateStatusTick(statusTick, msg_data['status']);
+  statusTick.setAttribute("id", msg_data['id']);
+
+  var msgcontent = document.createTextNode(msg_data['data']);
+  var timecontent = document.createTextNode(timeFromTs(msg_data['ts']));
 
   textDiv.append(msgcontent);
   timeSpan.append(timecontent);
@@ -121,162 +112,160 @@ function createSentBubble(msg_data){
 
   var mylist = document.getElementById("conversation");
   mylist.appendChild(msgBodyDiv);
-      
+
 }
 
 
-function createDateHeader_forHistory(msg_data,previous_date){
-        var current_date=dateNow(msg_data['ts']);
-        // console.log(current date,msg_data['data'],msg_data['ts']);
+function createDateHeader_forHistory(msg_data, previous_date) {
+  var current_date = dateNow(msg_data['ts']);
+  // console.log(current date,msg_data['data'],msg_data['ts']);
 
-        if(previous_date!=current_date){
-          previous_date = current_date;
-          // createDateHeaderBlock(current_date);
-          console.log("Making header for date"); 
-            
-            if(current_date == dateNow(+ new Date()))
-              createDateHeaderBlock("Today");
-            else if(current_date == dateYesterday(+ new Date()))
-              createDateHeaderBlock("Yesterday");
-            else
-              createDateHeaderBlock(current_date);
-          }
-          
-        return previous_date;
+  if (previous_date != current_date) {
+    previous_date = current_date;
+    // createDateHeaderBlock(current_date);
+    console.log("Making header for date");
+
+    if (current_date == dateNow(+new Date()))
+      createDateHeaderBlock("Today");
+    else if (current_date == dateYesterday(+new Date()))
+      createDateHeaderBlock("Yesterday");
+    else
+      createDateHeaderBlock(current_date);
+  }
+
+  return previous_date;
 }
 
-function createDateHeaderBlock(date_value){
-          var iDiv=document.createElement('div');
-          iDiv.className="row message-previous";
-          var innerDiv=document.createElement('div');
-          innerDiv.className="col-sm-12 previous";
-          var headerDiv=document.createElement('div');
-          headerDiv.className='date_header';
-          var datetext=document.createTextNode(date_value);
-          
-          headerDiv.append(datetext);
-          innerDiv.append(headerDiv);
-          iDiv.append(innerDiv);
+function createDateHeaderBlock(date_value) {
+  var iDiv = document.createElement('div');
+  iDiv.className = "row message-previous";
+  var innerDiv = document.createElement('div');
+  innerDiv.className = "col-sm-12 previous";
+  var headerDiv = document.createElement('div');
+  headerDiv.className = 'date_header';
+  var datetext = document.createTextNode(date_value);
 
-          var mylist = document.getElementById("conversation")
-          mylist.appendChild(iDiv);
+  headerDiv.append(datetext);
+  innerDiv.append(headerDiv);
+  iDiv.append(innerDiv);
+
+  var mylist = document.getElementById("conversation")
+  mylist.appendChild(iDiv);
 }
 
-function getUserFromPhone(phone_number,phone_book){
-        console.log(phone_number,phone_book);
-        user_list = Object.keys(phone_book);
-       
-
-        for (var i = 0 ;i< user_list.length;i++){
-        console.log(phone_book[user_list[i]]['phone'] == phone_number)          
-          if(phone_book[user_list[i]]['phone'] == phone_number)
-            return user_list[i];
-        }
-
-        console.log("User does not exist with phone phone number",phone_number);
-        return -1;
-      }
+function getUserFromPhone(phone_number, phone_book) {
+  console.log(phone_number, phone_book);
+  user_list = Object.keys(phone_book);
 
 
-function updateStatusTick(statusTick,status){
-    if(statusTick){
+  for (var i = 0; i < user_list.length; i++) {
+    console.log(phone_book[user_list[i]]['phone'] == phone_number)
+    if (phone_book[user_list[i]]['phone'] == phone_number)
+      return user_list[i];
+  }
 
-    switch(status){
+  console.log("User does not exist with phone phone number", phone_number);
+  return -1;
+}
 
-    case MESIBO_MSGSTATUS_SENT:
-      statusTick.setAttribute("src","images/whatsapp_single_tick.png");
-      break;
 
-    case MESIBO_MSGSTATUS_DELIVERED:
-      statusTick.setAttribute("src","images/whatsapp_double_tick.png");
-      break;
+function updateStatusTick(statusTick, status) {
+  if (statusTick) {
 
-    
-    case MESIBO_MSGSTATUS_READ:
-      statusTick.setAttribute("src","images/whatsapp_double_tick_coloured.png");
-      break;
+    switch (status) {
 
-    default:
-      statusTick.setAttribute("src","images/ic_av_timer.png");
+      case MESIBO_MSGSTATUS_SENT:
+        statusTick.setAttribute("src", "images/whatsapp_single_tick.png");
+        break;
+
+      case MESIBO_MSGSTATUS_DELIVERED:
+        statusTick.setAttribute("src", "images/whatsapp_double_tick.png");
+        break;
+
+
+      case MESIBO_MSGSTATUS_READ:
+        statusTick.setAttribute("src", "images/whatsapp_double_tick_coloured.png");
+        break;
+
+      default:
+        statusTick.setAttribute("src", "images/ic_av_timer.png");
 
     }
   }
 
 }
 
-function updateScroll(){
+function updateScroll() {
   var objDiv = document.getElementById("conversation");
   objDiv.scrollTop = objDiv.scrollHeight;
 }
 
-function updateProfilePic(user_name,file_path){
-  document.getElementById(user_name).setAttribute("src",file_path);
+function updateProfilePic(user_name, file_path) {
+  document.getElementById(user_name).setAttribute("src", file_path);
 }
 
-function updateLastMsg(selected_user_name,selected_user_id){
-  var lastMsgId = String(selected_user_name)+"_LastMsg";
-  var lastMsgDateId = String(selected_user_name)+"_LastDate";
-  var lastMsgStatusId = String(selected_user_name)+"_LastStatus";
+function updateLastMsg(selected_user_name, selected_user_id) {
+  var lastMsgId = String(selected_user_name) + "_LastMsg";
+  var lastMsgDateId = String(selected_user_name) + "_LastDate";
+  var lastMsgStatusId = String(selected_user_name) + "_LastStatus";
 
-  var lastMsgArea=document.getElementById(lastMsgId);
+  var lastMsgArea = document.getElementById(lastMsgId);
   $(lastMsgId).text('');
-  var lastMsgDateArea=document.getElementById(lastMsgDateId);
+  var lastMsgDateArea = document.getElementById(lastMsgDateId);
   $(lastMsgDateId).text('');
-  var lastMsgStatusArea=document.getElementById(lastMsgStatusId);
+  var lastMsgStatusArea = document.getElementById(lastMsgStatusId);
   // console.log(lastMsgStatusArea);
 
   var retrievedMsgArray = localStorage.getItem(selected_user_id);
-  retrievedMsgArray=JSON.parse(retrievedMsgArray);
+  retrievedMsgArray = JSON.parse(retrievedMsgArray);
 
-  console.log("Last Message",retrievedMsgArray);
-  if(retrievedMsgArray){
-  console.log("Last Message",retrievedMsgArray);
+  console.log("Last Message", retrievedMsgArray);
+  if (retrievedMsgArray) {
+    console.log("Last Message", retrievedMsgArray);
 
-  var lastMsgContent=retrievedMsgArray[retrievedMsgArray.length -1];
-  if(lastMsgContent['data'].length > 20)
-      lastMsgArea.innerHTML=lastMsgContent['data'].slice(0,20) + " ...";
-  else
-      lastMsgArea.innerHTML=lastMsgContent['data'];
-  
-
-  if(dateNow(lastMsgContent['ts']) == dateYesterday(+ new Date()))
-    lastMsgDateArea.innerHTML='Yesterday';
-  else if(dateNow(lastMsgContent['ts']) == dateNow(+ new Date()))
-    lastMsgDateArea.innerHTML=timeNow2(lastMsgContent['ts']);
-  else
-    lastMsgDateArea.innerHTML=dateNow(lastMsgContent['ts']);
+    var lastMsgContent = retrievedMsgArray[retrievedMsgArray.length - 1];
+    if (lastMsgContent['data'].length > 20)
+      lastMsgArea.innerHTML = lastMsgContent['data'].slice(0, 20) + " ...";
+    else
+      lastMsgArea.innerHTML = lastMsgContent['data'];
 
 
-  if(lastMsgContent['flag']==3){ //Message Recieved, Don't show status tick
-    lastMsgStatusArea.style.display = "none";
-  }
-  else {
-    lastMsgStatusArea.style.display = "inline";
-    updateStatusTick(lastMsgStatusArea,lastMsgContent['status']);
+    if (dateNow(lastMsgContent['ts']) == dateYesterday(+new Date()))
+      lastMsgDateArea.innerHTML = 'Yesterday';
+    else if (dateNow(lastMsgContent['ts']) == dateNow(+new Date()))
+      lastMsgDateArea.innerHTML = timeFromTs(lastMsgContent['ts']);
+    else
+      lastMsgDateArea.innerHTML = dateFormat(new Date(lastMsgContent['ts']), "dd/mm/yyyy");
+
+
+    if (lastMsgContent['flag'] == 3) { //Message Recieved, Don't show status tick
+      lastMsgStatusArea.style.display = "none";
+    } else {
+      lastMsgStatusArea.style.display = "inline";
+      updateStatusTick(lastMsgStatusArea, lastMsgContent['status']);
     }
   }
 
 }
 
-function getLastTs(selected_user_id){
+function getLastTs(selected_user_id) {
   var retrievedMsgArray = localStorage.getItem(selected_user_id);
 
-  retrievedMsgArray=JSON.parse(retrievedMsgArray);
-  if(retrievedMsgArray){
-  console.log(retrievedMsgArray);
-  var lastMsgContent=retrievedMsgArray[retrievedMsgArray.length - 1];
-  return lastMsgContent['ts'];
-  }
-  else
-    return 0 ;
+  retrievedMsgArray = JSON.parse(retrievedMsgArray);
+  if (retrievedMsgArray) {
+    console.log(retrievedMsgArray);
+    var lastMsgContent = retrievedMsgArray[retrievedMsgArray.length - 1];
+    return lastMsgContent['ts'];
+  } else
+    return 0;
 }
 
-function updateUserListOrder(phone_book){
+function updateUserListOrder(phone_book) {
   var $divs = $("#UserList .row.sideBar-body");
-  var TimeOrderedDivs = $divs.sort(function(a,b){
-        return getLastTs(phone_book[b.id]['phone']) - getLastTs((phone_book[a.id]['phone']));
-    });
-    $("#UserList").html(TimeOrderedDivs);
+  var TimeOrderedDivs = $divs.sort(function(a, b) {
+    return getLastTs(phone_book[b.id]['phone']) - getLastTs((phone_book[a.id]['phone']));
+  });
+  $("#UserList").html(TimeOrderedDivs);
 }
 
 Array.prototype.contains = function(v) {
@@ -295,4 +284,3 @@ Array.prototype.unique = function() {
   }
   return arr;
 }
-
